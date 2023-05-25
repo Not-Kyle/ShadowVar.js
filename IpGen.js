9cfc86e9ac48a0a36b399f42a227323a13c72fce
/Immortal <77679412+Not-Kyle@users.noreply.github.com>
2023-05-25 02:45:30 -0400

    Create IpGen.js

---
+++ IpGen.js
@@ -0,0 +1,5 @@
+let ipgen = Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255)
+
+console.log(ipgen);
+
+ipgen = 172061505; //the value of total IP's you can generate from this code.
